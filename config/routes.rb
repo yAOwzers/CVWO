4409092dc41d37c1bc6099f0166eb9914cd625f7
Joel <jo.neoyj@gmail.com>
2020-12-25 15:12:00 +0800

    Fixed typo error in reviews_controller and POST successful

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  resources :todo_lists do
-    resources :todo_items
-  end
+  root "todos#index"
+  get "todos/all_todos"
+  put "todos/update"
+  # get 'todos/index'
+  # root "welcome#index"
+  # get 'welcome/index'
+  # resources :todo_lists do
+  #   resources :todo_items
+  # end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_20_035014) do
+ActiveRecord::Schema.define(version: 2020_12_25_040515) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,13 @@ ActiveRecord::Schema.define(version: 2020_12_20_035014) do
 
   create_table "todo_lists", force: :cascade do |t|
     t.string "title"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "todos", force: :cascade do |t|
+    t.string "title"
+    t.boolean "completed"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
